c40ae616c7f186a0cf56d417b01240ff304dd8b1
Wesam <wesamsoulaiman@gmail.com>
2024-09-03 17:54:39 +0200

    URL updated

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,10 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api/auth"
+    : "/api/auth";
 axios.defaults.withCredentials = true; // put cookies in request header
 
 export const useAuthStore = create((set) => ({
